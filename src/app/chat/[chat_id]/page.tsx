3762973d685eb022951bf01d807dd597097fbc1e
alicesainta-sketch <alicesainta@gmail.com>
2026-01-31 16:55:37 +0800

    输入框及提交 完成聊天页ui

--- src/app/chat/[chat_id]/page.tsx
+++ src/app/chat/[chat_id]/page.tsx
@@ -2,15 +2,21 @@
 
 import { useChat } from "@ai-sdk/react";
 import { useEffect, useRef, useState } from "react";
+import EastIcon from "@mui/icons-material/East";
 
 export default function Page() {
   const [input, setInput] = useState("");
   const { messages, sendMessage } = useChat({});
+  const [model, setModel] = useState("deepseek-v3");
+  const handleChangeModel = () => {
+    setModel(model === "deepseek-v3" ? "deepseek-r1" : "deepseek-v3");
+  };
 
   const endRef = useRef<HTMLDivElement>(null);
+  const messageCount = messages?.length ?? 0;
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages?.length ?? 0]);
+  }, [messageCount]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -52,13 +58,46 @@ export default function Page() {
 
         <div className="h-4" ref={endRef}></div>
 
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <input
             name="prompt"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <button type="submit">Submit</button>
+        </form> */}
+
+        {/* 输入框 */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center mt-4 shadow-lg border-[1px] border-gray-300 h-32 rounded-lg"
+        >
+          <textarea
+            className="w-full rounded-lg p-3 h-30 focus:outline-none"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
+          <div className="flex flex-row items-center justify-between w-full h-12 mb-2">
+            <div>
+              <div
+                className={`flex flex-row items-center justify-center rounded-lg border-[1px]
+            px-2 py-1 ml-2 cursor-pointer ${
+              model === "deepseek-r1"
+                ? "border-blue-300 bg-blue-200"
+                : "border-gray-300"
+            }`}
+                onClick={handleChangeModel}
+              >
+                <p className="text-sm">深度思考（R1）</p>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full"
+            >
+              <EastIcon />
+            </button>
+          </div>
         </form>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import EastIcon from "@mui/icons-material/East";
 
 export default function Home() {
+  const router = useRouter();
   const [input, setInput] = useState("");
   const [model, setModel] = useState("deepseek-v3");
+  const handleChangeModel = () => {
+    setModel(model === "deepseek-v3" ? "deepseek-r1" : "deepseek-v3");
+  };
+  const handleSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    router.push(`/chat/${crypto.randomUUID()}`);
+  };
 
   return (
     <div className="h-screen flex flex-col items-center">
       <div className="h-1/5"></div>
       <div className="w-1/2">
         <p className="font-bold text-2xl text-center">Welcome to DeepScan!</p>
-        <div className="flex flex-col items-center justify-center mt-4 shadow-lg border-[1px] border-gray-300 h-32 rounded-lg">
+        <div className="flex flex-col items-center justify-center mt-4 shadow-lg border-[1px] border-gray-300 h-32 rounded-lg w-2/3">
           <textarea
             className="w-full rounded-lg p-3 h-30 focus:outline-none"
             value={input}
@@ -24,11 +32,15 @@ export default function Home() {
               <div
                 className={`flex flex-row items-center justify-center rounded-lg border-[1px]
             px-2 py-1 ml-2 cursor-pointer ${model === "deepseek-r1" ? "border-blue-300 bg-blue-200" : "border-gray-300"}`}
+                onClick={handleChangeModel}
               >
                 <p className="text-sm">深度思考（R1）</p>
               </div>
             </div>
-            <div className="flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full">
+            <div
+              className="flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full"
+              onClick={handleSubmit}
+            >
               <EastIcon></EastIcon>
             </div>
           </div>
